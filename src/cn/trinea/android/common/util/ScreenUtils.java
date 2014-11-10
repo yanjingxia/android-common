f135ec385b7cd279a672b13feb057e5aebfc08f3
baokunfu <baokunfu@gmail.com>
2014-11-10 11:29:46 +0800

    add method to ScreenUtils

--- src/cn/trinea/android/common/util/ScreenUtils.java
+++ src/cn/trinea/android/common/util/ScreenUtils.java
@@ -39,4 +39,98 @@ public class ScreenUtils {
     public static float pxToDpCeilInt(Context context, float px) {
         return (int)(pxToDp(context, px) + 0.5f);
     }
+    
+    /**
+     * 将px值转换为dip或dp值，保证尺寸大小不变
+     * 
+     * @param pxValue
+     * @param scale
+     *            （DisplayMetrics类中属性density）
+     * @return
+     */
+    public static int px2dip(Context context, float pxValue) {
+        if (context == null) {
+            return -1;
+        }
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 将dip或dp值转换为px值，保证尺寸大小不变
+     * 
+     * @param dipValue
+     * @param scale
+     *            （DisplayMetrics类中属性density）
+     * @return
+     */
+    public static int dip2px(Context context, float dipValue) {
+        if (context == null) {
+            return -1;
+        }
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    /**
+     * 传入dip资源id
+     * @param context
+     * @param dipResId
+     * @return
+     */
+    public static int dip2px(Context context, int dipResId) {
+        if (context == null) {
+            return -1;
+        }
+        float dipValue = context.getResources().getDimension(dipResId);
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     * 
+     * @param pxValue
+     * @param fontScale
+     *            （DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        if (context == null) {
+            return -1;
+        }
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     * 
+     * @param spValue
+     * @param fontScale
+     *            （DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        if (context == null) {
+            return -1;
+        }
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+    
+    /**
+     * 传入sp的资源
+     * @param context
+     * @param resId
+     * @return
+     */
+    public static int sp2px(Context context, int spResId) {
+        if (context == null) {
+            return -1;
+        }
+        float spValue = context.getResources().getDimension(spResId);
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
 }
